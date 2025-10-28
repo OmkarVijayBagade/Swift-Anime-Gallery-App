32bf34027be4644b1fd442ddd078b280faed9e8f
omkar <omkarbagade16@gmail.com>
2025-10-28 15:46:24 +0530

    Initial Commit

---
+++ AnimeGallery/AnimeGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeGalleryApp.swift
+//  AnimeGallery
+//
+//  Created by Omkar Vijay Bagade on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimeGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
